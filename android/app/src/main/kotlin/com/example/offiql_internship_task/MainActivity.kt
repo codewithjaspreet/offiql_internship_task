107e2e703ca62bb0322a559facf6b2611b2cce8b
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-06-16 07:00:48 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/offiql_internship_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.offiql_internship_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color cardColor = Colors.green;
